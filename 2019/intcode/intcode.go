c1fe4faa1411a19b00ddf666a6627b29de8f9fd3
Ian McMahon <facetious.ian@gmail.com>
2019-12-10 00:14:27 -0500

    Day 9, intcode gets new instructions

--- 2019/intcode/intcode.go
+++ 2019/intcode/intcode.go
@@ -12,15 +12,18 @@ type ParamMode int
 const (
 	POSITION  ParamMode = 0
 	IMMEDIATE           = 1
+	RELATIVE            = 2
 )
 
 // gets the param at ip, fetched based on mode
-func getParam(input []int, ip int, mode ParamMode) int {
+func getParam(memory []int, ip int, rb int, mode ParamMode) int {
 	switch mode {
 	case POSITION:
-		return input[input[ip]]
+		return memory[memory[ip]]
 	case IMMEDIATE:
-		return input[ip]
+		return memory[ip]
+	case RELATIVE:
+		return memory[rb+memory[ip]]
 	default:
 		panic(fmt.Errorf("Unknown parameter mode: %d", mode))
 	}
@@ -29,13 +32,13 @@ func getParam(input []int, ip int, mode ParamMode) int {
 
 // splits the parameter modes value into A and B input modes
 // turns out the "output" digit is the leading 0 of the opcode, badly documented
-func iioParamModes(modes int) (a, b ParamMode, err error) {
+func iioParamModes(modes int) (a, b, o ParamMode, err error) {
 	a = ParamMode(modes % 10)
 	modes /= 10
 	b = ParamMode(modes % 10)
-	modes /= 10
-	if modes == IMMEDIATE {
-		err = fmt.Errorf("modes %d, 3rd param (output) should never be immediate", modes)
+	o = ParamMode(modes / 10)
+	if o == IMMEDIATE {
+		err = fmt.Errorf("modes %d, 3rd param (output) should never be immediate", o)
 	}
 	return
 }
@@ -50,10 +53,10 @@ func Process(program []int, userInput ...int) (memory, output []int) {
 		wg.Add(1)
 		for {
 			o, more := <-outCh
-			output = append(output, o)
 			if !more {
 				break
 			}
+			output = append(output, o)
 		}
 		wg.Done()
 	}()
@@ -72,13 +75,14 @@ func Process(program []int, userInput ...int) (memory, output []int) {
 }
 
 func Run(program []int, input <-chan int, output chan<- int, console io.Writer) (memory []int) {
-	memory = make([]int, len(program))
+	memory = make([]int, 1024*1024)
 	copy(memory, program)
 	defer close(output)
 
 	ip := 0
+	rb := 0
 	for {
-		// fmt.Printf("ip: %d, instruction: %d \n", ip, memory[ip])
+		//fmt.Printf("ip: %d, instruction: %d \n", ip, memory[ip])
 
 		instruction := memory[ip]
 		opcode := instruction % 100
@@ -87,64 +91,72 @@ func Run(program []int, input <-chan int, output chan<- int, console io.Writer) 
 		switch opcode {
 		case 99:
 			// HALT
-			fmt.Fprintf(console, "HALT\n")
+			//fmt.Fprintf(console, "HALT\n")
 			return memory
 
 		case 1:
 			// ADD
-			am, bm, err := iioParamModes(paramModes)
+			am, bm, om, err := iioParamModes(paramModes)
 			if err != nil {
 				fmt.Printf("ip: %d instruction: %d err: %v\n", ip, instruction, err)
 			}
 			// getparam dereferences the pointers if appropriate
-			a, b := getParam(memory, ip+1, am), getParam(memory, ip+2, bm)
+			a, b := getParam(memory, ip+1, rb, am), getParam(memory, ip+2, rb, bm)
 			// output is always a pointer
 			po := memory[ip+3]
+			// but sometimes relative
+			if om == RELATIVE {
+				po += rb
+			}
 
 			memory[po] = a + b
 			ip += 4
 
 		case 2:
 			// MUL
-			am, bm, err := iioParamModes(paramModes)
+			am, bm, om, err := iioParamModes(paramModes)
 			if err != nil {
 				fmt.Printf("ip: %d instruction: %d err: %v\n", ip, instruction, err)
 			}
 			// getparam dereferences the pointers if appropriate
-			a, b := getParam(memory, ip+1, am), getParam(memory, ip+2, bm)
+			a, b := getParam(memory, ip+1, rb, am), getParam(memory, ip+2, rb, bm)
 			// output is always a pointer
 			po := memory[ip+3]
+			// but sometimes relative
+			if om == RELATIVE {
+				po += rb
+			}
 
 			memory[po] = a * b
 			ip += 4
 
 		case 3:
 			// INPUT
-			// writes to the single param, so never immediate mode
+			// this needs to handle relative mode
 			pa := memory[ip+1]
+			if paramModes%10 == RELATIVE {
+				fmt.Printf("the dreaded 203: rb %d + %d\n", rb, pa)
+				pa += rb
+			}
 			// block on input channel until we have input
-			fmt.Fprintf(console, "blocking on input\n")
+			//fmt.Fprintf(console, "blocking on input\n")
 			memory[pa] = <-input
-			fmt.Fprintf(console, "got input %d\n", memory[pa])
+			fmt.Fprintf(console, "stored at %d input %d\n", pa, memory[pa])
 			ip += 2
 
 		case 4:
 			// OUTPUT
-			// single param can be position or immediate
-			a := getParam(memory, ip+1, ParamMode(paramModes%10)) // mode shouldn't ever be > 10 but :)
-			fmt.Fprintf(console, "blocking on output\n")
+			a := getParam(memory, ip+1, rb, ParamMode(paramModes%10))
+			//fmt.Fprintf(console, "blocking on output\n")
 			output <- a
-			fmt.Fprintf(console, "sent output %d\n", a)
+			//fmt.Fprintf(console, "sent output %d\n", a)
 			ip += 2
 
 		case 5:
 			// JNZ
-			am, bm, err := iioParamModes(paramModes)
-			if err != nil {
-				fmt.Printf("ip: %d instruction: %d err: %v\n", ip, instruction, err)
-			}
+			am, bm, _, _ := iioParamModes(paramModes)
 			// getparam dereferences the pointers if appropriate
-			a, b := getParam(memory, ip+1, am), getParam(memory, ip+2, bm)
+			a, b := getParam(memory, ip+1, rb, am), getParam(memory, ip+2, rb, bm)
 			if a != 0 {
 				ip = b
 			} else {
@@ -153,12 +165,9 @@ func Run(program []int, input <-chan int, output chan<- int, console io.Writer) 
 
 		case 6:
 			// JZ
-			am, bm, err := iioParamModes(paramModes)
-			if err != nil {
-				fmt.Printf("ip: %d instruction: %d err: %v\n", ip, instruction, err)
-			}
+			am, bm, _, _ := iioParamModes(paramModes)
 			// getparam dereferences the pointers if appropriate
-			a, b := getParam(memory, ip+1, am), getParam(memory, ip+2, bm)
+			a, b := getParam(memory, ip+1, rb, am), getParam(memory, ip+2, rb, bm)
 			if a == 0 {
 				ip = b
 			} else {
@@ -167,14 +176,18 @@ func Run(program []int, input <-chan int, output chan<- int, console io.Writer) 
 
 		case 7:
 			// LT    C = (A < B ? 1 : 0)
-			am, bm, err := iioParamModes(paramModes)
+			am, bm, om, err := iioParamModes(paramModes)
 			if err != nil {
 				fmt.Printf("ip: %d instruction: %d err: %v\n", ip, instruction, err)
 			}
 			// getparam dereferences the pointers if appropriate
-			a, b := getParam(memory, ip+1, am), getParam(memory, ip+2, bm)
+			a, b := getParam(memory, ip+1, rb, am), getParam(memory, ip+2, rb, bm)
 			// output is always a pointer
 			po := memory[ip+3]
+			// but sometimes relative
+			if om == RELATIVE {
+				po += rb
+			}
 			if a < b {
 				memory[po] = 1
 			} else {
@@ -184,20 +197,30 @@ func Run(program []int, input <-chan int, output chan<- int, console io.Writer) 
 
 		case 8:
 			// EQ    C = (A == B ? 1 : 0)
-			am, bm, err := iioParamModes(paramModes)
+			am, bm, om, err := iioParamModes(paramModes)
 			if err != nil {
 				fmt.Printf("ip: %d instruction: %d err: %v\n", ip, instruction, err)
 			}
 			// getparam dereferences the pointers if appropriate
-			a, b := getParam(memory, ip+1, am), getParam(memory, ip+2, bm)
+			a, b := getParam(memory, ip+1, rb, am), getParam(memory, ip+2, rb, bm)
 			// output is always a pointer
 			po := memory[ip+3]
+			// but sometimes relative
+			if om == RELATIVE {
+				po += rb
+			}
 			if a == b {
 				memory[po] = 1
 			} else {
 				memory[po] = 0
 			}
 			ip += 4
+
+		case 9:
+			// RB OFFSET
+			a := getParam(memory, ip+1, rb, ParamMode(paramModes%10))
+			rb += a
+			ip += 2
 
 		default:
 			fmt.Printf("Bad opcode at instruction %d: %d\n", ip, memory[ip])
